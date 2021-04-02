9723996a68cb09ba3b74a2a5ea88f2b36b90d978
Érico <eriicoalan@gmail.com>
2021-04-02 12:14:33 -0300

    Atualizando backend

---
+++ src/database/migrations/1617374450969-AddProviderIdAppointmentRelationUser.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class AddProviderIdAppointmentRelationUser1617374450969
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+  }
+}
